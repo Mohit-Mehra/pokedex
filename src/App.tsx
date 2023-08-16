3ea1acd08558c76f891ddfeaebce5404e07d24e3
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-08-16 22:09:49 +0530

    background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Navbar from './sections/Navbar'
+import Wraper from './sections/Wraper'
+import Footer from './sections/Footer'
+import Background from './components/Background'
+import "./scss/index.scss"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='main-coinatiner'>
+      <Background />
+      <div className="app">
+        <Navbar />
+        <Wraper />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
---
+++ src/sections/Wraper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Wraper = () => {
+  return (
+    <div className='content'>Wraper</div>
+  )
+}
+
+export default Wraper
