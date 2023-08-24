eac838ab9821765b60e5fac51ba1dd93b831bc63
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-08-24 21:40:32 +0530

    Logout Complete

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,13 +1,23 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { MdOutlinePowerSettingsNew } from "react-icons/md";
+import { firebaseAuth } from "../utils/FirebaseConfig";
+import { useAppDispatch } from "../app/hooks";
+import { setToast, setUserStatus } from "../app/slices/AppSlice";
 
 const Footer = () => {
+  const dispatch = useAppDispatch()
+  const handleLogOut = () => {
+    signOut(firebaseAuth);
+    dispatch(setUserStatus(undefined))
+    dispatch(setToast("Logout Successfully...."))
+  };
   return (
     <footer>
       <div className="block"></div>
       <div className="data"></div>
       <div className="block">
-        <MdOutlinePowerSettingsNew />
+        <MdOutlinePowerSettingsNew onClick={handleLogOut} />
       </div>
     </footer>
   );
